5df6f15e186ea9623b838aa4ddc09d3e4b0128a3
Dweep Panchal <dweep.programmer@gmail.com>
2018-12-31 17:20:23 +0530

    Registration Added

--- app/src/main/java/in/fairshare/Activities/LoginActivity.java
+++ app/src/main/java/in/fairshare/Activities/LoginActivity.java
@@ -1,16 +1,29 @@
 package in.fairshare.Activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import in.fairshare.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser mUser;
 
     private EditText emailLogin;
     private EditText passwordLogin;
@@ -35,5 +48,75 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                mUser = firebaseAuth.getCurrentUser();
+
+                if (mUser != null) {
+                    Toast.makeText(LoginActivity.this, "Signed In!!", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Not Signed In!!", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!TextUtils.isEmpty(emailLogin.getText().toString()) &&
+                        !TextUtils.isEmpty(passwordLogin.getText().toString())) {
+
+                    String email = emailLogin.getText().toString();
+                    String pwd = passwordLogin.getText().toString();
+
+                    login(email, pwd);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Please enter valid Email and Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void login(String email, String pwd) {
+
+        mAuth.signInWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            //  We are in!!!
+                            Toast.makeText(LoginActivity.this, "Login!!", Toast.LENGTH_LONG).show();
+
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Incorrect Email & Password!!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener); // To See User State whether it is login or not
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
--- app/src/main/java/in/fairshare/Fragments/ProfileFragment.java
+++ app/src/main/java/in/fairshare/Fragments/ProfileFragment.java
@@ -10,35 +10,39 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import in.fairshare.Activities.LoginActivity;
 import in.fairshare.R;
 
 public class ProfileFragment extends Fragment {
 
-//    private FirebaseUser mUser;
-//    private FirebaseAuth mAuth;
-//
-//    private TextView signoutText;
+    private FirebaseUser mUser;
+    private FirebaseAuth mAuth;
+
+    private TextView signoutText;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile,container,false);
 
-//        signoutText = view.findViewById(R.id.signoutTextID);
-//
-//        mAuth = FirebaseAuth.getInstance();
-//        mUser = mAuth.getCurrentUser();
-//
-//        signoutText.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                if(mUser != null && mAuth != null) {
-//                    mAuth.signOut();
-//                    startActivity(new Intent(getContext(), LoginActivity.class));
-//                }
-//            }
-//        });
+        signoutText = view.findViewById(R.id.signoutTextID);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        signoutText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mUser != null && mAuth != null) {
+                    mAuth.signOut();
+                    startActivity(new Intent(getContext(), LoginActivity.class));
+                }
+            }
+        });
         return view;
     }
 }
